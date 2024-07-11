fe67becf471ed819325a17363315231ee2c4c75c
Yuliia Fito <jylia.fito2004@gmail.com>
2024-07-11 10:39:56 +0300

    make build

---
+++ index.0eac85be.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.querySelector(".gallery div img");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");r&&(e.preventDefault(),t.setAttribute("src",r.href))});
+//# sourceMappingURL=index.0eac85be.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const ul = document.querySelector("ul");
+const largeImg = document.querySelector(".gallery div img");
+ul.addEventListener("click", (e)=>{
+    const link = e.target.closest(".list-item__link");
+    if (!link) return;
+    e.preventDefault();
+    largeImg.setAttribute("src", link.href);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
